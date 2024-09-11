2836bcc04aebb2a24fb55fac7b34320083232129
gooplet0075 <79529592+gooplet0075@users.noreply.github.com>
2024-09-11 18:42:51 -0400

    create v2.0

---
+++ v2.0/badges.js
@@ -0,0 +1,100 @@
+var globalBadges = { //badges require OAUTH. for now, i'm hard-coding in a list of global badges
+    "broadcaster": {
+      "1": "https://static-cdn.jtvnw.net/badges/v1/5527c58c-fb7d-422d-b71b-f309dcb85cc1/1"
+    },
+    "moderator": {
+      "1": 'https://static-cdn.jtvnw.net/badges/v1/3267646d-33f0-4b17-b3df-f923a41db1d0/1'
+    },
+    "vip": {
+      "1": 'https://static-cdn.jtvnw.net/badges/v1/b817aba4-fad8-49e2-b88a-7cc744dfa6ec/1'
+    },
+    "subscriber": { //custom sub badges
+      "0": "",
+      "2": "",
+      "3": "",
+      "6": "",
+      "9": "",
+      "12": "",
+      "2000": "",
+      "2002": "",
+      "2003": "",
+      "2006": "",
+      "2009": "",
+      "2012": "",
+      "3000": "",
+      "3002": "",
+      "3003": "",
+      "3006": "",
+      "3009": "",
+      "3012": ""
+    },
+    "bits": { //custom bit badges
+      "100": "",
+      "1000": "",
+      "5000": "",
+      "10000": "",
+      "25000": ""
+    }
+};
+
+const customBadges = {
+    "_template": {
+        "subscriber": { //custom sub badges
+        "0": "",
+        "2": "",
+        "3": "",
+        "6": "",
+        "9": "",
+        "12": "",
+        "2000": "",
+        "2002": "",
+        "2003": "",
+        "2006": "",
+        "2009": "",
+        "2012": "",
+        "3000": "",
+        "3002": "",
+        "3003": "",
+        "3006": "",
+        "3009": "",
+        "3012": ""
+        },
+        "bits": { //custom bit badges
+        "100": "",
+        "1000": "",
+        "5000": "",
+        "10000": "",
+        "25000": ""
+        }
+    },
+    "gogoGooplet": {
+        "subscriber": { //custom sub badges
+          "0": "https://static-cdn.jtvnw.net/badges/v1/a38de92d-e58b-4257-a482-636091d9365f/1",
+          "2": "https://static-cdn.jtvnw.net/badges/v1/91b26c72-e926-48e6-83b8-fefb411f9cff/1",
+          "3": "https://static-cdn.jtvnw.net/badges/v1/558125cd-de8d-424e-9b9e-c03980c15095/1",
+          "6": "https://static-cdn.jtvnw.net/badges/v1/a8ad3d5a-611f-4328-8e84-47f3e0d7f776/1",
+          "9": "https://static-cdn.jtvnw.net/badges/v1/3fc71c48-67f9-49ed-a431-17c08fa95df9/1",
+          "12": "https://static-cdn.jtvnw.net/badges/v1/49bf073d-63a5-4587-ab8b-f96abc7e2e0c/1",
+          "2000": "https://static-cdn.jtvnw.net/badges/v1/6f4e97e5-8e63-4a1d-b2a2-f83514d3413e/1",
+          "2002": "https://static-cdn.jtvnw.net/badges/v1/05a850e3-2fe9-4865-a98a-b9021f71575b/1",
+          "2003": "https://static-cdn.jtvnw.net/badges/v1/ff76cd8e-e066-4ee8-85d1-c39c1c1ff377/1",
+          "2006": "https://static-cdn.jtvnw.net/badges/v1/c51ed094-24d7-415e-b208-53477b7ff95b/1",
+          "2009": "https://static-cdn.jtvnw.net/badges/v1/08cf1a49-db29-425e-8ef5-6054c7482cf5/1",
+          "2012": "https://static-cdn.jtvnw.net/badges/v1/1753fba4-9c40-493d-b6bc-2848d758c893/1",
+          "3000": "https://static-cdn.jtvnw.net/badges/v1/fa25da4d-8f69-417f-bd0e-6a212b21b9a2/1",
+          "3002": "https://static-cdn.jtvnw.net/badges/v1/7bd47c69-9ba8-4999-8153-7df8d1ffd44a/1",
+          "3003": "https://static-cdn.jtvnw.net/badges/v1/b3d6ff9b-dd20-4d0d-86b0-446d58da3134/1",
+          "3006": "https://static-cdn.jtvnw.net/badges/v1/0c87ba3a-e721-4fc8-a22e-1739886e261c/1",
+          "3009": "https://static-cdn.jtvnw.net/badges/v1/8b47d950-8b51-458c-b178-ca02c57c0c8b/1",
+          "3012": "https://static-cdn.jtvnw.net/badges/v1/7a15e107-beee-40a6-ab1b-b9bc5046ce41/1"
+        },
+        "bits": { //custom bit badges
+          "100": "https://static-cdn.jtvnw.net/badges/v1/96b4009c-0e37-4b88-b679-77baa323e99f/1",
+          "1000": "https://static-cdn.jtvnw.net/badges/v1/195c5988-b762-4d7a-87a1-d3a1ba737082/1",
+          "5000": "https://static-cdn.jtvnw.net/badges/v1/cc61a53b-dc2b-4c72-b427-2b0942aa7779/1",
+          "10000": "https://static-cdn.jtvnw.net/badges/v1/45a4c7b1-c9d4-4fd1-ba55-f81fc8207390/1",
+          "25000": "https://static-cdn.jtvnw.net/badges/v1/68164a82-f2f9-466f-816e-91462a7be552/1"
+        }
+    },
+
+}
---
+++ v2.0/chat-overlay.js
@@ -0,0 +1,415 @@
+console.log('version: v2.0');
+
+// handle query parameters
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+var twitchChannel = urlParams.get('twitchChannel');
+var badges;
+if (twitchChannel) {
+    // create twitch chat badges object from twitchChannel name, if channel name is listed
+    var badges = globalBadges;
+    if (twitchChannel && (twitchChannel in customBadges)) {
+    badges.bits = customBadges[twitchChannel].bits;
+    badges.subscriber = customBadges[twitchChannel].bits;
+    };
+};
+
+var youtubeChannel = urlParams.get('youtubeChannel');
+var youtubeChannelId = urlParams.get('youtubeChannelId');
+var youtubeVideoId = urlParams.get('youtubeVideoId');
+var youtubeChatId = urlParams.get('youtubeChatId');
+//var gapiKey = urlParams.get('gapiKey');
+var gapiKey = urlParams.get('key');
+
+var youtubeModeratorColor = urlParams.get('youtubeModeratorColor');
+var youtubeOwnerColor = urlParams.get('youtubeOwnerColor');
+var youtubeSponsorColor = urlParams.get('youtubeSponsorColor');
+var youtubeVerifiedColor = urlParams.get('youtubeVerifiedColor');
+var youtubeDefaultColor = urlParams.get('youtubeDefaultColor');
+
+var youtubeRoleColors = {
+  'chatModerator':'#5e84f1',
+  'chatOwner':'#ffca28',
+  'chatSponsor':'#2ba640',
+  'verified':'#58acf2',
+  'default':'#b7b7b7'
+}
+console.log('key = ',gapiKey);
+if (gapiKey) {
+    gapiKey = '&key=' + gapiKey;
+    gapiURL = 'https://youtube.googleapis.com/youtube/v3/';
+} else {
+    gapiKey = ''
+    gapiURL = 'https://yt.lemnoslife.com/noKey/';
+}
+
+var youtubeChat = [];
+
+async function getYoutubeChannelId() { // returns id of channel (quota: 100)
+    if (!youtubeChannelId) {
+        console.log('fetching youtubeChannelId...');
+        const requestURL = `${gapiURL}search?part=snippet&q=${youtubeChannel}${gapiKey}&type=channel`
+        console.log(requestURL);
+        const response = await fetch(requestURL);
+        console.log(response);
+        const json = await response.json();
+        console.log(json);
+        youtubeChannelId = json.items[0].id.channelId;
+    } else {console.log('getYoutubeChannelId call not made; channelId=', youtubeChannelId)};
+    return youtubeChannelId;
+}
+
+async function getYoutubeLiveBroadcastId() { // returns videoId of current live broadcast (quota: 100)
+  console.log('in getYoutubeLiveBroadcastId() ')
+  if (!youtubeVideoId) {
+      const response = await fetch(`${gapiURL}search?part=snippet&channelId=${youtubeChannelId}&order=date&type=video&eventType=live${gapiKey}`);
+      const json = await response.json();
+      console.log(json);
+      youtubeVideoId = json.items[0].id.videoId;
+  } else {console.log('getYoutubeLiveBroadcaseId call not made; videoId=', youtubeVideoId)};
+  return youtubeVideoId;
+}
+
+async function getYoutubeLiveChatId() { //returns live chat ID (quota: 1)
+    console.log(`in getYoutubeLiveChatId(); chatId = [${youtubeChatId}]`);
+    if (!youtubeChatId) {
+        const response = await fetch(`${gapiURL}videos?part=liveStreamingDetails,snippet&id=${youtubeVideoId}${gapiKey}`);
+        const json = await response.json();
+        youtubeChatId = await json.items[0].liveStreamingDetails.activeLiveChatId;
+    } else {console.log('getYoutubeLiveChatId call not made; chatId=', youtubeChatId)};
+    console.log(`activeLiveChatId: ${youtubeChatId}`);
+    return youtubeChatId;
+}
+
+async function getYoutubeChat(pageToken='') { // returns page of chat (quota: ???)
+    //console.log('line 51 pageToken=' + pageToken);
+    if (pageToken) {
+        pageToken = '&pageToken=' + pageToken;
+    };
+    const request = `${gapiURL}liveChat/messages?liveChatId=${youtubeChatId}&part=snippet,authorDetails${pageToken}${gapiKey}`
+    const response = await fetch(request);
+    const json = await response.json();
+    const interval = json.pollingIntervalMillis;
+    const nextPageToken = json.nextPageToken;
+    //console.log('next page token', nextPageToken)
+    const totalResults = json.pageInfo.totalResults;
+    var incompleteMessageList = false;
+    if (json.pageInfo.resultsPerPage < json.pageInfo.totalResults) {
+        incompleteMessageList = true;
+    };
+    var messages = [];
+    for (item of json.items) {
+      var authorColor;
+      if (item.authorDetails.isChatOwner) {
+        authorColor = youtubeRoleColors.chatOwner;
+      } else if (item.authorDetails.isChatModerator) {
+        authorColor = youtubeRoleColors.chatModerator;
+      } else if (item.authorDetails.isChatSponsor) {
+        authorColor = youtubeRoleColors.chatSponsor;
+      } else if (item.authorDetails.isVerified) {
+        authorColor = youtubeRoleColors.verified;
+      } else {authorColor = youtubeRoleColors.default};
+      messages.push({
+          "id": item.id,
+          "userName": item.authorDetails.displayName,
+          "content": item.snippet.displayMessage,
+          "authorColor": authorColor,
+          "details": item
+      });
+    };
+    var messageResponse = {
+        "incompleteMessageList": incompleteMessageList,
+        "interval": interval,
+        "nextPage": nextPageToken,
+        "messages": messages,
+        "totalResults": totalResults
+    };
+    return messageResponse;
+}
+
+async function newMessage(message) { // creates html element with message
+    var newMessage = document.createElement('li');
+    newMessage.setAttribute('id', message.id);
+    var chatBox = document.querySelector("#chat>ul");
+    var text = document.createElement("blockquote");
+
+    var content = message.content;
+    console.log(message.userName + ': ' + content)
+    
+
+    // sanitize html
+    content = content.replace(/&/g, "&amp;").replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;").replace(/"/g, "&quot;");
+
+    var contentCopy = content;
+    
+    for (const [key,value] of Object.entries(youtubeEmotes)) {
+      content = content.split(key).join(`<img src=${value}>`);
+      contentCopy = contentCopy.split(key).join(' ');
+    };
+
+    var isTokiPonaMessage = await isTokiPona(contentCopy);
+
+    
+
+    // split message into segments for different formatting, if message is in toki pona 
+    if (isTokiPonaMessage[0] && contentCopy === contentCopy.toLowerCase()) {
+        // this message is in toki pona, and has no proper nouns. 
+        // therefore we can apply basic toki pona formatting
+        console.log('toki pona w/o proper nouns');
+
+        var span = document.createElement('span');
+        span.className = 'tokipona';
+        span.innerHTML = content;
+        text.appendChild(span);
+    } else if (isTokiPonaMessage[0]) {
+        console.log('toki pona w/ proper nouns');
+        // this message is in toki pona, and has proper nouns
+        var uniqueNouns = [... new Set(isTokiPonaMessage[1])];
+        var properNouns = [];
+        for (word of uniqueNouns) {
+            if (word !== word.toLowerCase()) {
+            properNouns.push(word);
+            }
+        }
+        for (word of properNouns) {
+            content = content.split(word).join(`<span class="propernoun">${word}</span>`);
+        }
+        var span = document.createElement('span');
+        span.className = 'tokipona';
+        span.innerHTML = content;
+        text.appendChild(span);
+    } else {
+        console.log('not toki pona');
+        // message does not conform to toki pona standards set in isTokiPona()
+        // apply no special toki pona formatting
+        text.innerHTML = content;
+    }
+
+    //text.innerText = content;
+    newMessage.innerHTML = `<span style="color:${message.authorColor}">${message.userName + ':'}</span>`;
+
+    //newMessage.innerText = message.userName;
+    newMessage.append(text);
+    chatBox.append(newMessage);
+}
+
+async function pushYoutubeChat(messages) { // pushes each new message to chat list, calls newMessage
+    //console.log(messages);
+    for (const message of messages) {
+        console.log('pushing message:', message.content)
+        if (!youtubeChat.includes(message.id)) {
+            youtubeChat.push(message.id);
+            newMessage(message);
+        };
+    };
+}
+
+async function waitForYoutubeChatId() {
+    if (!youtubeVideoId) {
+      await getYoutubeChannelId(youtubeChannel);
+    };
+    console.log(`${youtubeChannel} channelId: ${youtubeChannelId}`);
+    while (!youtubeVideoId) {
+        console.log('trying to get live videoId...');
+        await getYoutubeLiveBroadcastId();
+        if (!youtubeVideoId) {await new Promise(r => setTimeout(r, 5000))};
+    };
+    console.log(`live videoId acquired: ${youtubeVideoId}`);
+    console.log(`chatId: ${youtubeChatId}`);
+    await getYoutubeLiveChatId();
+    console.log(`chatId: ${youtubeChatId}`);
+    return youtubeChatId;
+}
+
+async function youtubeChatListener() {
+    //var response = await getChat(chatId);
+    console.log('starting youtubeChatListener. chatId=', youtubeChatId);
+    var interval = 0;
+    var pageToken = '';
+    //console.log('line132 pageToken=', pageToken)
+    i = 1;
+    j = 0;
+    var ignoreList = [];
+    while (i) {
+        console.log(`listener loop# ${i}; pollingIntervalMillis: ${interval}; seconds: ${interval/1000}; pageToken: ${pageToken}`)
+        await new Promise(r => setTimeout(r, interval));
+        response = await getYoutubeChat(pageToken);
+        //console.log(response);
+        pushYoutubeChat(response.messages);
+        interval = response.interval;
+        pageToken = response.nextPage;
+        for (message of response.messages) {
+          if (message.content.includes('!clear') && !ignoreList.includes(message.id)) {
+            pageToken = '';
+            ignoreList.push(message.id);
+          }
+        }
+        if (pageToken === '' && j > 0) {
+          //console.log('pageToken ',pageToken, 'response',response,'youtubeChat',youtubeChat)
+          var tempYoutubeChat = [];
+          for (message of response.messages) {
+            tempYoutubeChat.push(message.id);
+          }
+          //console.log('ytchat',youtubeChat,'tempytchat',tempYoutubeChat)
+          for (id of youtubeChat) {
+            //console.log(id);
+            if (!tempYoutubeChat.includes(id)) {
+              var deletedMessage = document.getElementById(id);
+              console.log('deleted message',id,deletedMessage.innerHTML);
+              deletedMessage.remove()
+            };
+          };
+          youtubeChat = tempYoutubeChat;
+          j = 0;
+        };
+        i++;
+        j++;
+    }
+}
+
+async function youtubeInit() {
+    if (!youtubeChatId) {
+        await waitForYoutubeChatId();
+        console.log('chatId=',youtubeChatId);
+    };
+    await youtubeChatListener();
+};
+
+if (youtubeChannel || youtubeChannelId || youtubeVideoId || youtubeChatId) {
+    youtubeInit();
+};
+
+ComfyJS.onChat = async (user, message, flags, self, extra) => {
+  var newMessage = document.createElement('li');
+  newMessage.setAttribute('id', extra.id);
+  var chat = document.querySelector("#chat>ul");
+  var text = document.createElement("blockquote");
+
+  // check if message has emotes
+  // define copy of message to run isTokiPona check
+  var messageCopy = message;
+
+  // define list for storing emotes to be replaced
+  const stringReplacements = [];
+
+  if (extra.messageEmotes) {
+    // strip emotes from messageCopy for use in isTokiPona check
+    // also store emotes in stringReplacements list
+    for (emote in extra.messageEmotes){
+      for (indexes of extra.messageEmotes[emote]) {
+        var fromTo = indexes.split('-');
+        var fromInt = parseInt(fromTo[0]);
+        var toInt = parseInt(fromTo[1]);
+        var emoteString = message.substring(fromInt, toInt + 1);
+        messageCopy = messageCopy.substring(0, fromInt) + ' '.repeat(toInt - fromInt + 1) + messageCopy.substring(toInt + 1);
+      };
+      stringReplacements.push({
+        stringToReplace: emoteString,
+        replacement: `<img src="https://static-cdn.jtvnw.net/emoticons/v2/${emote}/default/dark/1.0">`,
+      });
+    };
+  };
+
+  // sanitize html
+  message = message.replace(/&/g, "&amp;").replace(/</g, "&lt;")
+  .replace(/>/g, "&gt;").replace(/"/g, "&quot;");
+  // replace emotes in original message with html
+  message = stringReplacements.reduce(
+    (acc, { stringToReplace, replacement }) => {
+      return acc.split(stringToReplace).join(replacement);
+    },
+    message
+  );
+  
+  // check if message is toki pona using messageCopy
+  var isTokiPonaMessage = await isTokiPona(messageCopy);
+  console.log('isTokiPonaMessage: ' + isTokiPonaMessage[0])
+  
+  // split message into segments for different formatting, if message is in toki pona 
+  if (isTokiPonaMessage[0] && messageCopy === messageCopy.toLowerCase()) {
+      // this message is in toki pona, and has no proper nouns. 
+      // therefore we can apply basic toki pona formatting
+      var span = document.createElement('span');
+      span.className = 'tokipona';
+      span.innerHTML = message;
+      text.appendChild(span);
+  } else if (isTokiPonaMessage[0]) {
+    // this message is in toki pona, and has proper nouns
+    var uniqueNouns = [... new Set(isTokiPonaMessage[1])];
+    var properNouns = [];
+    for (word of uniqueNouns) {
+      if (word !== word.toLowerCase()) {
+        properNouns.push(word);
+      }
+    }
+    for (word of properNouns) {
+      message = message.split(word).join(`<span class="propernoun">${word}</span>`);
+    }
+    var span = document.createElement('span');
+    span.className = 'tokipona';
+    span.innerHTML = message;
+    text.appendChild(span);
+  } else {
+    // message does not conform to toki pona standards set in isTokiPona()
+    // apply no special toki pona formatting
+    text.innerHTML = message;
+  }
+
+  var userBadges = '';
+  for (badge in extra['userBadges']) {
+    if (badge in badges) {
+      var badgeID = extra['userBadges'][badge];
+      if (badgeID in badges[badge]) {
+        userBadges += `<img src="${badges[badge][badgeID]}">`;   
+      }
+    }
+  }
+
+  newMessage.innerHTML = `<span style="color:${extra.userColor}">${userBadges + user + ':'}</span>`;
+  newMessage.append(text);
+  chat.append(newMessage);
+}
+
+//this is silly, but i've repurposed the above code here just to get commands to display in chat
+ComfyJS.onCommand = async (user, message, flags, self, extra) => {
+  var newMessage = document.createElement('li');
+  newMessage.setAttribute('id', extra.id);
+  var chat = document.querySelector("#chat>ul");
+  var text = document.createElement("blockquote");
+  message = '!' + message + ' ' + flags;
+  
+  // sanitize html
+  message = message.replace(/&/g, "&amp;").replace(/</g, "&lt;")
+  .replace(/>/g, "&gt;").replace(/"/g, "&quot;");
+
+  text.innerText = message;
+
+  var userBadges = '';
+  for (badge in extra['userBadges']) {
+    if (badge in badges) {
+      var badgeID = extra['userBadges'][badge];
+      if (badgeID in badges[badge]) {
+        userBadges += `<img src="${badges[badge][badgeID]}">`;   
+      }
+    }
+  }
+
+  newMessage.innerHTML = `<span style="color:${extra.userColor}">${userBadges + user + ':'}</span>`;
+  newMessage.append(text);
+  chat.append(newMessage);
+}
+
+ComfyJS.onMessageDeleted = async (id, extra) => {
+  var deletedMessage = document.getElementById(id);
+  deletedMessage.remove()
+}
+
+if (twitchChannel) {
+    ComfyJS.Init(twitchChannel)
+}
+
+// references:
+//https://gist.github.com/w3cj/4f1fa02b26303ae1e0b1660f2349e705
+
+//https://yt.lemnoslife.com/ (keyless api)
